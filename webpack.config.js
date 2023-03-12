93ea3cc56fae624f645c5a9fdfc27bb358c54017
James <james.bradford1@gmail.com>
2023-03-12 20:27:50 +0000

    Init Project Toolchain

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/build/",
+    filename: "bundle.js"
+  },
+}
